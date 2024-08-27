4a3ea9bf1edf8210ea8f978f5bea9ce18294588f
rahul09999 <rahullalwani102002@gmail.com>
2024-08-27 23:53:47 +0530

    Feat: Setup view engine

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 7000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// Set view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
 app.get('/', (req, res) => {
-    return res.send("Sup from server...");
+    return res.render("welcome");
 });
 app.listen(PORT, () => console.log(`Server is running on port: localhost:${PORT}`));
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,21 @@
 import express, { Application, Request, Response } from 'express';
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 import 'dotenv/config'
 
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Set view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
 app.get('/', (req:Request, res: Response) => {
-    return res.send("Sup from server...")
+    return res.render("welcome");
 })
 
 app.listen(PORT,()=> console.log(`Server is running on port: localhost:${PORT}`) )
